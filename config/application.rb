fd2910fcc3bff3db9a289049da3efb597419aa7b
Daniel Ishola <danielishola@Daniels-MacBook-Pro.local>
2017-07-29 15:53:54 +0100

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ Bundler.require(*Rails.groups)
 module OlaBlog
   class Application < Rails::Application
 
-config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-I18n.available_locales = [:en, :zh, :fr]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
