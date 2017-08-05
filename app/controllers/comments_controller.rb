185c30ea972aebb3455b88a7e4221b4104cb3ef3
Daniel Ishola <danielishola@Daniels-MacBook-Pro.local>
2017-08-05 15:46:18 +0100

    modify comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,20 @@
+
 class CommentsController < ApplicationController
   def create
-  @article = Article.find(params[:article_id])
-  @comment = @article.comments.create(comment_params)
-  if @comment.save
-      respond_to do |format|
-        format.html { redirect_to article_path(@comment.article), notice: 'Thanks for your comment'}
-        format.js
-    else
-      respond_to do |format|
-        format.html { redirect_to @comment.article, alert: 'Unable to add comment' }
-        format.js { render 'fail_create.js.erb' }
-      end
-    end
-end
-
-
-def destroy
-  @article = Article.find(params[:article_id])
-  @comment = @article.comments.find(params[:id])
-  @comment.destroy
-  respond_to do |format|
-      format.html { redirect_to article_path(@comment.article), notice: 'Comment Deleted' }
-      format.js
-    end
-end
-
-private
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
   end
-end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
